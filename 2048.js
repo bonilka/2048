774bf1b0fa7d0b89fa2216c650d850f1aae420f8
Nikita Karpov <bonilka@gmail.com>
2015-08-13 11:00:13 +0300

    move is work

--- 2048.js
+++ 2048.js
@@ -1,10 +1,21 @@
 var Cell = function(value) {
     this.value = value || undefined;
-
+    this.set = function (value) {
+        this.value = value;
+    }
 }
-var Row = function() {
-    this.row = [new Cell(4), new Cell(4), new Cell(8), new Cell(16)];
-    this.horizontalLength = this.row.length;
+var Row = function(cellVaules) {
+    // this.init = function () {
+        this.row = [];
+        if(cellVaules){
+            for (var i = 0; i < cellVaules.length; i++) {
+                this.row.push(new Cell(cellVaules[i]));
+            }
+        }else{
+            this.row = [new Cell, new Cell, new Cell, new Cell];
+        }
+        this.horizontalLength = this.row.length;
+    // }
 
     function newNumber() {
         var min = 1;
@@ -29,11 +40,11 @@ var Row = function() {
             for (var i = 0; i < missingCellAmount; i++) {
                 switch(side){
                     case 'left':
-                        this.row.push(new Cell);
+                        this.row.unshift(new Cell);
                         break;
                     case 'right':
                     default:
-                        this.row.unshift(new Cell);
+                        this.row.push(new Cell);
                 }
             }
         }
@@ -42,7 +53,7 @@ var Row = function() {
         this.deleteEmptyCell();
         for (var i = 0; i < this.row.length-1; i++) {
             var nextIndex = i + 1;
-            console.log("i="+i,this.row[i].value);
+            // console.log("i="+i,this.row[i].value);
             if (nextIndex <= this.row.length) {
                 if (this.row[i].value !== undefined && this.row[nextIndex].value == this.row[i].value) {
                     this.row[i].value += this.row[nextIndex].value;
@@ -65,7 +76,13 @@ var Row = function() {
     this.right = function() {
         this.deleteEmptyCell();
         this.rightSum();
-        this.addEmptyCell();
+        this.addEmptyCell('left');
+    }
+    this.set = function(array){
+        for (var i = 0; i < this.row.length; i++) {
+            this.row[i].value = array[i];
+        }
+        return this.getArray();
     }
     this.toString = function() {
         var output = this.getArray();
@@ -77,73 +94,110 @@ var Row = function() {
             output.push(cell.value);
         })
         return output;
-    }    
+    }
 }
+
 // var test = [[1,2],[3,4],[5,6],[7,8]];
 
 //console.log(test);
-/*var Matrix2048 = function(colNum) {
-    this.value = [new Row, new Row, new Row, new Row];
+var Matrix2048 = function(colNum) {
+    this.matrix = [new Row([undefined,4,2,undefined]), new Row([4,8,2,2]), new Row([undefined,8,undefined,undefined]), new Row([undefined,16,4,undefined])];
 
     this.getCol = function (colNum) {
-        var col = [];
-        for (var i = 0; i < this.value.length; i++) {
-            col.push(this.value[i].value[colNum].value);
+        var col;
+        var colValues = [];
+        for (var i = 0; i < this.matrix.length; i++) {
+            // console.log(this.matrix[i].row[colNum].value);
+            colValues.push(this.matrix[i].row[colNum].value);
         }
+        col = new Row(colValues);
         return col;
     }
-    this.getValue = function() {
-        var output = [];
-        this.value.forEach(function(row, i, rows) {
-            output.push(row.getValue());
-        });
-        //console.log(output.toString());
-        return output;
+    this.refreshCol = function (value,colNum){
+        for (var i = 0; i < this.matrix.length; i++) {
+            this.matrix[i].row[colNum].set(value[i]);
+        }
+    }
+    this.upCol = function (colNum) {
+        var col = new Row(this.getCol(colNum).getArray());
+        col.left();
+        this.refreshCol(col.getArray(), colNum);
+        return this.getMatrix();
+    }
+    this.downCol = function (colNum) {
+        var col = new Row(this.getCol(colNum).getArray());
+        col.right();
+        this.refreshCol(col.getArray(), colNum);
+        return this.getMatrix();
+    }
+    this.up = function () {
+        for (var i = 0; i < this.matrix.length; i++) {
+            this.upCol(i);
+        }
+        return this.getMatrix();
+    }
+    this.down = function () {
+        for (var i = 0; i < this.matrix.length; i++) {
+            this.downCol(i);
+        }
+        return this.getMatrix();
     }
     this.right = function () {
-        this.value.forEach(function (row, i, rows) {
+        this.matrix.forEach(function (row, i, rows) {
             row.right();
         })
     }
     this.left = function () {
-        this.value.forEach(function (row, i, rows) {
+        this.matrix.forEach(function (row, i, rows) {
             row.left();
         })
-    }
-    this.upOne = function (colNum) {
-        var col = new Row(this.getCol(colNum));
-
-
-    }
-    this.up = function () {
-
     }
     this.move = function(moveTo) {
         switch (moveTo) {
             case 'right':
                 this.right();
                 console.log('move to ' + moveTo);
-                console.log('Now matrix is ' + this.getValue());
                 break
             case 'left':
                 this.left();
                 console.log('move to ' + moveTo);
-                console.log('Now matrix is ' + this.getValue());
                 break
             case 'up':
-                // this.upCol(1);
+                this.up();
                 console.log('move to ' + moveTo);
                 break
             case 'down':
+                this.down();
                 console.log('move to ' + moveTo);
                 break
             default:
                 console.log('impossible move to ' + moveTo);
         }
     }
+    this.getMatrix = function () {
+        var output = [];
+        this.matrix.forEach(function(row, i, rows) {
+            output.push(row.getArray());
+        });
+        //console.log(output.toString());
+        return output;
+    }
+    this.toString = function () {
+        var output = this.getMatrix();
+        return output.toString();
+    }
+
+    /*
+
+
+
+    this.up = function () {
+
+    }
+*/
 }
 
-var row1 = new Row;
+/*var row1 = new Row;
 // console.log(row1.getValue());
 var matrix = new Matrix2048;
 matrix.getValue();
