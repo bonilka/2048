c2781af5a2bc6a64c21d6b291fa6dae9b16e6cc3
Nikita Karpov <bonilka@gmail.com>
2015-08-13 15:00:26 +0300

    add styles, add correct add new numeric

--- 2048.js
+++ 2048.js
@@ -1,27 +1,27 @@
 var Cell = function(value) {
     this.value = value || undefined;
-    this.set = function (value) {
+    this.set = function(value) {
         this.value = value;
     }
 }
 var Row = function(cellVaules) {
     // this.init = function () {
-        this.row = [];
-        if(cellVaules){
-            for (var i = 0; i < cellVaules.length; i++) {
-                this.row.push(new Cell(cellVaules[i]));
-            }
-        }else{
-            this.row = [new Cell, new Cell, new Cell, new Cell];
+    this.row = [];
+    if (cellVaules) {
+        for (var i = 0; i < cellVaules.length; i++) {
+            this.row.push(new Cell(cellVaules[i]));
         }
-        this.horizontalLength = this.row.length;
+    } else {
+        this.row = [new Cell, new Cell, new Cell, new Cell];
+    }
+    this.horizontalLength = this.row.length;
     // }
 
-    this.addNewNumeric = function (value) {
+    this.addNewNumeric = function(value) {
         min = 0;
-        max = this.horizontalLength-1;
+        max = this.horizontalLength - 1;
         var cellNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        if(this.row[cellNum].value === undefined){
+        if (this.row[cellNum].value === undefined) {
             this.row[cellNum].set(value);
             return true
         }
@@ -29,18 +29,18 @@ var Row = function(cellVaules) {
 
 
     }
-    this.deleteEmptyCell = function () {
+    this.deleteEmptyCell = function() {
         for (var i = this.row.length - 1; i >= 0; i--) {
             if (this.row[i].value === undefined) {
                 this.row.splice(i, 1);
             }
         }
     }
-    this.addEmptyCell = function (side) {
+    this.addEmptyCell = function(side) {
         var missingCellAmount = this.horizontalLength - this.row.length;
-        if(missingCellAmount != 0){
+        if (missingCellAmount != 0) {
             for (var i = 0; i < missingCellAmount; i++) {
-                switch(side){
+                switch (side) {
                     case 'left':
                         this.row.unshift(new Cell);
                         break;
@@ -53,7 +53,7 @@ var Row = function(cellVaules) {
     }
     this.leftSum = function() {
         this.deleteEmptyCell();
-        for (var i = 0; i < this.row.length-1; i++) {
+        for (var i = 0; i < this.row.length - 1; i++) {
             var nextIndex = i + 1;
             // console.log("i="+i,this.row[i].value);
             if (nextIndex <= this.row.length) {
@@ -80,15 +80,15 @@ var Row = function(cellVaules) {
         this.rightSum();
         this.addEmptyCell('left');
     }
-    this.set = function(array){
+    this.set = function(array) {
         for (var i = 0; i < this.row.length; i++) {
             this.row[i].value = array[i];
         }
         return this.getArray();
     }
-    this.isFull = function () {
-        for (var i = 0; i < this.row.length ; i++) {
-            if(this.row[i].value === undefined){
+    this.isFull = function() {
+        for (var i = 0; i < this.row.length; i++) {
+            if (this.row[i].value === undefined) {
                 return false
             }
         }
@@ -98,7 +98,7 @@ var Row = function(cellVaules) {
         var output = this.getArray();
         return output.toString();
     }
-    this.getArray = function () {
+    this.getArray = function() {
         var output = [];
         this.row.forEach(function(cell, i, row) {
             output.push(cell.value);
@@ -111,10 +111,12 @@ var Row = function(cellVaules) {
 
 //console.log(test);
 var Matrix2048 = function(colNum) {
-    this.matrix = [new Row([undefined,undefined,2,undefined]), new Row([undefined,undefined,2,undefined]), new Row([undefined,undefined,undefined,undefined]), new Row([undefined,undefined,undefined,undefined])];
+    this.matrix = [new Row([undefined, undefined, 2, undefined]), new Row([undefined, undefined, 2, undefined]), new Row([undefined, undefined, undefined, undefined]), new Row([undefined, undefined, undefined, undefined])];
     this.verticalLength = this.matrix.length;
     this.horizontalLength = this.matrix[0].row.length;
-    this.getCol = function (colNum) {
+    this.prevMove = '';
+    this.prevSumIndex = 12;
+    this.getCol = function(colNum) {
         var col;
         var colValues = [];
         for (var i = 0; i < this.matrix.length; i++) {
@@ -124,46 +126,46 @@ var Matrix2048 = function(colNum) {
         col = new Row(colValues);
         return col;
     }
-    this.refreshCol = function (value,colNum){
+    this.refreshCol = function(value, colNum) {
         for (var i = 0; i < this.matrix.length; i++) {
             this.matrix[i].row[colNum].set(value[i]);
         }
     }
-    this.upCol = function (colNum) {
+    this.upCol = function(colNum) {
         var col = new Row(this.getCol(colNum).getArray());
         col.left();
         this.refreshCol(col.getArray(), colNum);
         return this.getMatrix();
     }
-    this.downCol = function (colNum) {
+    this.downCol = function(colNum) {
         var col = new Row(this.getCol(colNum).getArray());
         col.right();
         this.refreshCol(col.getArray(), colNum);
         return this.getMatrix();
     }
-    this.up = function () {
+    this.up = function() {
         for (var i = 0; i < this.matrix.length; i++) {
             this.upCol(i);
         }
         return this.getMatrix();
     }
-    this.down = function () {
+    this.down = function() {
         for (var i = 0; i < this.matrix.length; i++) {
             this.downCol(i);
         }
         return this.getMatrix();
     }
-    this.right = function () {
-        this.matrix.forEach(function (row, i, rows) {
+    this.right = function() {
+        this.matrix.forEach(function(row, i, rows) {
             row.right();
         })
     }
-    this.left = function () {
-        this.matrix.forEach(function (row, i, rows) {
+    this.left = function() {
+        this.matrix.forEach(function(row, i, rows) {
             row.left();
         })
     }
-    this.newNumber = function () {
+    this.newNumber = function() {
         var min = 1;
         var max = 2;
         var probably = Math.random();
@@ -173,27 +175,48 @@ var Matrix2048 = function(colNum) {
             return 4
         }
     }
-    this.isFull = function () {
+    this.isFull = function() {
         for (var i = 0; i < this.matrix.length; i++) {
-            if(!this.matrix[i].isFull()){
+            if (!this.matrix[i].isFull()) {
                 return false;
             }
         }
         return true
     }
-    this.addNumeric = function () {
+    this.addNumeric = function() {
         var min = 0;
-        var max = this.verticalLength-1;
+        var max = this.verticalLength - 1;
         var rowNum = Math.floor(Math.random() * (max - min + 1)) + min;
-        while(!this.matrix[rowNum].addNewNumeric(this.newNumber())){
+        while (!this.matrix[rowNum].addNewNumeric(this.newNumber())) {
             rowNum = Math.floor(Math.random() * (max - min + 1)) + min;
-            if(this.isFull()){
+            if (this.isFull()) {
                 console.log('Game Over');
                 break;
             }
         }
     }
+    this.matrixInLine = function() {
+        return this.matrix.toString().split(',').map(function(item) {
+            if (item != false) {
+                return parseInt(item);
+            } else {
+                return undefined;
+            }
+        });
+    }
+    this.sumFilledIndex = function() {
+        var sum = 0;
+        var arr = this.matrixInLine();
+        arr.forEach(function(el, i) {
+            if (el !== undefined) {
+                sum += el + i;
+            }
+        });
+        return sum;
+    }
     this.move = function(moveTo) {
+        var currentFilledIndexSum = 0;
+        this.prevSumIndex = this.sumFilledIndex();
         switch (moveTo) {
             case 'right':
                 this.right();
@@ -214,39 +237,36 @@ var Matrix2048 = function(colNum) {
             default:
                 console.log('impossible move to ' + moveTo);
         }
-        this.addNumeric();
+        this.isWin();
+        currentFilledIndexSum = this.sumFilledIndex();
+        if (this.prevSumIndex != currentFilledIndexSum) {
+            this.prevMove = moveTo;
+            this.addNumeric();
+        }
+
     }
-    this.getMatrix = function () {
+    this.isWin = function() {
+        var arr = this.matrixInLine()
+        var that = this;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == 2048) {
+                that.move = function() {}
+                console.log('You win!!!');
+                return true;
+            }
+        }
+
+    }
+    this.getMatrix = function() {
         var output = [];
         this.matrix.forEach(function(row, i, rows) {
             output.push(row.getArray());
         });
         return output;
     }
-    this.toString = function () {
+    this.toString = function() {
         var output = this.getMatrix();
         return output.toString();
     }
 
-    /*
-
-
-
-    this.up = function () {
-
-    }
-*/
 }
-
-/*var row1 = new Row;
-// console.log(row1.getValue());
-var matrix = new Matrix2048;
-matrix.getValue();
-// row1.left();
-//
-// row1.right();
-// console.log(row1.getValue());
-matrix.move('up');
-// matrix.move('left');
-console.log(matrix.getValue());
-*/
--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-var matrix =  new Matrix2048;
-var refreshTable = function () {
+var matrix = new Matrix2048;
+var refreshTable = function() {
     var arr = matrix.toString().split(',');
-    $('td').each(function(index, el) {
-        if (!arr[index]) {
-            $(el).text("X");
-        }else{
+    $('.table2048 td').each(function(index, el) {
+            if (!arr[index]) {
+                $(el).removeClass().text("");
 
-            $(el).text(arr[index]);
-        }
+            } else {
+                $(el).removeClass().addClass('color-'+arr[index]).text(arr[index]);
+            }
+
     });
 }
 $(document).on('keydown', function(event) {
     // event.preventDefault();
-    if (event.which==37) {
+    if (event.which == 37) {
         matrix.move('left');
         refreshTable();
     }
-    if (event.which==38) {
+    if (event.which == 38) {
         matrix.move('up');
         refreshTable();
     }
-    if (event.which==39) {
+    if (event.which == 39) {
         matrix.move('right');
         refreshTable();
     }
-    if (event.which==40) {
+    if (event.which == 40) {
         matrix.move('down');
         refreshTable();
     }
